504d9e8805889c7c41cf01e617b5cfa3ee722d18
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-04-20 09:40:29 +0200

    fix: handle exceptions

--- export-to-markdown.js
+++ export-to-markdown.js
@@ -5,62 +5,77 @@ const { cleanHTML } = require("./cleanHTML");
 const exec = require("child_process").exec;
 
 const baseFolder = "1FpnENOl1oZXLzmvvIqrR3kJgPNsGaDTo";
+const exportDir = [process.cwd(), "export"];
 
 /**
- * Lists the names and IDs of up to 10 files.
+ * Lists the names and IDs of up to 100 files.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-const listFiles = (parentFolder, parents = []) => (auth) => {
-  const drive = google.drive({ version: "v3", auth });
-  drive.files.list(
-    {
-      q: `"${parentFolder}" in parents and (mimeType = 'application/vnd.google-apps.folder' or mimeType = 'application/vnd.google-apps.document')`,
-      pageSize: 10,
-      fields: "nextPageToken, files(id, name, mimeType)",
-    },
-    (err, res) => {
-      if (err) return console.log("The API returned an error: " + err);
-      const files = res.data.files;
-      if (files.length) {
-        files.map((file) => {
-          if (file.mimeType === "application/vnd.google-apps.document") {
-            exportFile(file.id, file.name, parents, auth);
+const listFiles = (parentFolder, parents = []) => async (auth) =>
+  new Promise((resolve, reject) => {
+    try {
+      const drive = google.drive({ version: "v3", auth });
+      drive.files.list(
+        {
+          q: `"${parentFolder}" in parents and (mimeType = 'application/vnd.google-apps.folder' or mimeType = 'application/vnd.google-apps.document')`,
+          pageSize: 100,
+          fields: "nextPageToken, files(id, name, mimeType)",
+        },
+        (err, res) => {
+          if (err) return reject("The API returned an error: " + err);
+          const files = res.data.files;
+          if (files.length) {
+            files.map((file) => {
+              if (file.mimeType === "application/vnd.google-apps.document") {
+                exportFile(file.id, file.name, parents, auth);
+              }
+              if (file.mimeType === "application/vnd.google-apps.folder") {
+                listFiles(file.id, [...parents, file.name.trim()])(auth);
+              }
+            });
           }
-          if (file.mimeType === "application/vnd.google-apps.folder") {
-            listFiles(file.id, [...parents, file.name.trim()])(auth);
-          }
-        });
-      }
+        }
+      );
+    } catch (error) {
+      reject(error);
     }
-  );
-};
+  });
 
-const exportFile = (fileId, name, parents, auth) => {
-  const drive = google.drive({ version: "v3", auth });
-  const folder = path.join(process.cwd(), "export", ...parents);
-  const outFile = path.join(folder, `${name}.md`);
-  fs.mkdirSync(folder, { recursive: true });
-  const dest = fs.createWriteStream(outFile);
-  drive.files.export(
-    {
-      fileId,
-      mimeType: "text/html",
-    },
-    async (err, res) => {
-      if (err) return console.log("The API returned an error: " + err);
-      const html = cleanHTML(res.data);
-      const markdown = await new Promise((resolve, reject) => {
-        const e = exec("pandoc -f html -t markdown_strict", (err, res) => {
-          if (err) return reject(err);
-          resolve(res);
-        });
-        e.stdin.write(html);
-        e.stdin.end();
-      });
-      await fs.promises.writeFile(outFile, markdown);
-      console.log(outFile, "written");
-    }
-  );
+const exportFile = async (fileId, name, parents, auth) => {
+  try {
+    const drive = google.drive({ version: "v3", auth });
+    const folder = path.join(...exportDir, ...parents);
+    const outFile = path.join(folder, `${name}.md`);
+    fs.mkdirSync(folder, { recursive: true });
+    return new Promise((resolve, reject) => {
+      drive.files.export(
+        {
+          fileId,
+          mimeType: "text/html",
+        },
+        async (err, res) => {
+          if (err) return reject("The API returned an error: " + err);
+          const html = cleanHTML(res.data);
+          const markdown = await new Promise((resolve, reject) => {
+            const e = exec("pandoc -f html -t markdown_strict", (err, res) => {
+              if (err) return reject(err);
+              resolve(res);
+            });
+            e.stdin.write(html);
+            e.stdin.end();
+          })
+            .then((markdown) => fs.promises.writeFile(outFile, markdown))
+            .then(() => {
+              console.log(outFile, "written");
+              resolve(outFile);
+            })
+            .catch(reject);
+        }
+      );
+    });
+  } catch (error) {
+    reject(error);
+  }
 };
 
 const main = async () => {
@@ -70,7 +85,7 @@ const main = async () => {
   });
   const client = await auth.getClient();
 
-  listFiles(baseFolder)(client);
+  await listFiles(baseFolder)(client);
 };
 
 main().catch((err) => {
