06aeefbfabf7e88d81bb4b235848ec4809845b7d
Markus Tacker <m@coderbyheart.com>
2020-05-30 22:27:51 +0200

    fix: hide scrollbar if not needed

--- src/navigation/DocumentNavigation.tsx
+++ src/navigation/DocumentNavigation.tsx
@@ -11,7 +11,7 @@ const LinkContainer = styled.div`
 	flex-direction: column;
 	max-height: 100%;
 	overflow: hidden;
-	overflow-y: scroll;
+	overflow-y: auto;
 `
 
 const NavigationWithMenu = styled.div``
